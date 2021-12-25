2a3a96ef806127627718dafca99cdc0e85bf63f9
jhhlim <31980053+jhhlim@users.noreply.github.com>
2021-12-24 17:33:30 -0800

    Create day8-dictionary.cpp

---
+++ day8-dictionary.cpp
@@ -0,0 +1,40 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+     std::map<string, string> phoneBook;
+    int n;
+    cin >> n;
+    //https://www.codingbroz.com/day-8-dictionaries-and-maps-solution/
+    // Read names and numbers, add to phoneBook:
+    for(int i = 0; i < n; i++){
+        string name;
+        string phone;
+        cin >> name;
+        cin >> phone;
+        phoneBook[name] = phone;
+    }
+    
+    // Execute queries:
+    std::map<string,string>::iterator it;
+    string query;
+    while( cin >> query ){
+        it = phoneBook.find(query);
+        
+        if ( it != phoneBook.end() ){ // key is found in phoneBook    
+            cout << it->first << "=" << it->second << '\n';
+        }
+        else{ // the iterator hit the end of the phone book without finding key
+            cout << "Not found" << '\n';
+        }
+    }
+    
+    return 0;
+}
